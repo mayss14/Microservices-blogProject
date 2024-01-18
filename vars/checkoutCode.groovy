8d9ffdac84c541041764312f887adf734806e8a2
Mayssae Jabbar <jabbaremayssae@gmail.com>
2024-01-18 20:46:42 +0100

    jenkins-shared-library

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -2,10 +2,10 @@ def call(String microserviceName, String repoUrl, String branch) {
     def workingDir = "${env.WORKSPACE}/${microserviceName}"
     
     // Clone the repository
-    sh "git clone ${repoUrl} ${workingDir}"
+    bat "git clone ${repoUrl} ${workingDir}"
     
     // Checkout the specified branch
-    sh "git -C ${workingDir} checkout ${branch}"
+    bat "git -C ${workingDir} checkout ${branch}"
     
     return workingDir
 }
